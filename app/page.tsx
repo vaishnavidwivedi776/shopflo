247e602b1616ac1c46b14364467523091db3cb01
vaishnvai dwivedi <vaishnavidwivedi776@gmail.com>
2026-07-17 11:26:29 +0530

    Task 1 completed

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,74 @@
-import Image from "next/image";
+"use client";
+
+import { useEffect, useState } from "react";
+
+type User = {
+  id: number;
+  firstName: string;
+  lastName: string;
+  age: number;
+  email: string;
+  company: {
+    name: string;
+    department: string;
+  };
+  address: {
+    city: string;
+  };
+};
 
 export default function Home() {
+  const [users, setUsers] = useState<User[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    fetch("https://dummyjson.com/users")
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error("Failed");
+        }
+        return res.json();
+      })
+      .then((data) => {
+        setUsers(data.users);
+        setLoading(false);
+      })
+      .catch(() => {
+        setError("Something went wrong!");
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) return <h1 className="text-center mt-10">Loading...</h1>;
+
+  if (error)
+    return <h1 className="text-center mt-10 text-red-500">{error}</h1>;
+
+  if (users.length === 0)
+    return <h1 className="text-center mt-10">No Employees Found</h1>;
+
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
+    <main className="p-8 bg-gray-100 min-h-screen">
+      <h1 className="text-3xl font-bold text-center mb-8">
+        Employee Listing
+      </h1>
+
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        {users.map((user) => (
+          <div key={user.id} className="bg-white p-5 rounded shadow">
+            <h2 className="text-xl font-bold">
+              {user.firstName} {user.lastName}
+            </h2>
+
+            <p>Email: {user.email}</p>
+            <p>Company: {user.company.name}</p>
+            <p>Department: {user.company.department}</p>
+            <p>City: {user.address.city}</p>
+            <p>Age: {user.age}</p>
+          </div>
+        ))}
+      </div>
+    </main>
   );
 }
